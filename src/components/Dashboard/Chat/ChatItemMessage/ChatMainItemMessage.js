ffb37c2629a7b32df837e7499f9da6ca609329c3
Ryzlane <ryzlane@free.fr>
2019-02-22 11:32:37 +0100

    handle if is user message or not

--- src/components/Dashboard/Chat/ChatItemMessage/ChatMainItemMessage.js
+++ src/components/Dashboard/Chat/ChatItemMessage/ChatMainItemMessage.js
@@ -3,14 +3,18 @@ import React, { Component } from 'react';
 class ChatMainItemMessage extends Component {
   render() {
     const { date, name, content } = this.props
+    const isUser = false
+    const isUserClass = isUser ? 'is-user-message' : ''
     return (
-      <div className='chat__item-message'>
-        <div className='chat__item-message__header'>
-          <span className="chat__item-message__header__name">{name}</span>
-        </div>
-        <div className="chat__item-message__content">
-          <p className='chat__item-message__content__message'>{content}</p>
-          <span className="chat__item-message__content__date">{date}</span>
+      <div className={`chat__item-message ${isUserClass}`}>
+        <div className='chat__item-message__container'>
+          <div className='chat__item-message__container__header'>
+            <span className="chat__item-message__container__header__name">{name}</span>
+          </div>
+          <div className="chat__item-message__container__content">
+            <p className='chat__item-message__container__content__message'>{content}</p>
+            <span className="chat__item-message__container__content__date">{date}</span>
+          </div>
         </div>
       </div>
     );
