96725e767d88fb4cfc9be4e6e4e1e2b1833f0e09
Ryzlane <ryzlane@free.fr>
2019-02-22 11:15:22 +0100

    fix style on messages

--- src/components/Dashboard/Chat/ChatItemMessage/ChatMainItemMessage.js
+++ src/components/Dashboard/Chat/ChatItemMessage/ChatMainItemMessage.js
@@ -7,9 +7,11 @@ class ChatMainItemMessage extends Component {
       <div className='chat__item-message'>
         <div className='chat__item-message__header'>
           <span className="chat__item-message__header__name">{name}</span>
-          <span className="chat__item-message__header__date">{date}</span>
         </div>
-        <p className='chat__item-message__content'>{content}</p>
+        <div className="chat__item-message__content">
+          <p className='chat__item-message__content__message'>{content}</p>
+          <span className="chat__item-message__content__date">{date}</span>
+        </div>
       </div>
     );
   }
