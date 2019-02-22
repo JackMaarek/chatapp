6390a741ffa5942be726ca057a9b4d5bc96aa16a
JackMaarek <jacques.maarek@hetic.net>
2019-02-22 13:01:46 +0100

    'MessageListBugFixed'

--- src/components/Dashboard/Chat/Chat.js
+++ src/components/Dashboard/Chat/Chat.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react';
 
 import MessageList from '../../MessageList';
 import ChatInput from './ChatInput/ChatInput';
-
-
 import ChatHeader from './ChatHeader/ChatHeader'
-import ChatInput from './ChatInput/ChatInput'
 
 class Chat extends Component {
   render() {
